973026ddbddf1f69dd006cb689dc2207467ababc
muhdamrullah <muneorion@gmail.com>
2016-02-11 10:17:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import render_template
 import subprocess
 import time
+import re
+import csv
 
 def createHotspot():
     subprocess.call('ifconfig wlan0 down', shell=True)
@@ -11,10 +13,25 @@ def createHotspot():
     time.sleep(1)
     subprocess.call('ifconfig wlan0 up', shell=True)
     time.sleep(1)
-    subprocess.call('airbase-ng -e OpenWifi -c 6 wlan0', shell=True)
-    time.sleep(60)
+    subprocess.Popen('airbase-ng -e OpenWifi -c 6 wlan0 >> test.csv', shell=True)
+    time.sleep(10)
+    searchMAC()
     subprocess.call('pkill airbase-ng', shell=True)
     time.sleep(1)
+
+def searchMAC():
+    for x in range (0,11):
+        with open('test.csv','rb') as myFile:
+            myFile.seek(-200,2)
+            last = myFile.readlines(1)[-1].decode()
+            searchFilter = 'Client'
+	    if searchFilter in last:
+                result = re.search("Client (.*) associated", last)
+                print result.group(1)
+	        time.sleep(5)
+	        break
+	    else:
+	        time.sleep(5)
 
 app = Flask(__name__)
 
