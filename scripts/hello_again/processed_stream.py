2de56be564743bb4ba4d9ddc541d9c551595305d
muhdamrullah <muneorion@gmail.com>
2016-02-15 23:15:17 +0800

    Update processed_stream.py

--- scripts/hello_again/processed_stream.py
+++ scripts/hello_again/processed_stream.py
@@ -42,14 +42,14 @@ while True:
 
         #if ((hour_right_now==16)&(minute_right_now<58)&(minute_right_now>=56)):
         if True:
-            raw1_file_list = glob.glob("../Raw/*.csv")
+            raw1_file_list = glob.glob("../airodump/*.csv")
             raw2_file_list= np.array([x.split("/")[2] if "kismet" not in x else "-99" for x in raw1_file_list])
             raw3_file_list = raw2_file_list[raw2_file_list!="-99"]
             index_file_list = np.array([int(((x.split("-")[1]).split("."))[0]) for x in raw3_file_list])
             file_list= np.array([y[1] for y in sorted(zip(index_file_list, raw3_file_list),key=lambda x:x[0])])
-            os.chdir("../Raw")
-            os.system("cat %s > ../Hello_Again/onelevel/real_time_combined_S.csv"%(cat_string(file_list)))
-            os.chdir("../Hello_Again/")
+            os.chdir("../airodump")
+            os.system("cat %s > ../hello_again/onelevel/real_time_combined_S.csv"%(cat_string(file_list)))
+            os.chdir("../hello_again/")
 
             with open('./onelevel/real_time_combined_S.csv','r') as in_file, open('./onelevel/full_day_S.csv','w') as out_file:
                 switch = 0
