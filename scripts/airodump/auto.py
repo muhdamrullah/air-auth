380fa1db9f960e457758e58fbd318e69ea611393
muhdamrullah <muneorion@gmail.com>
2016-02-15 22:54:21 +0800

    Create auto.py

---
+++ scripts/airodump/auto.py
@@ -0,0 +1,7 @@
+import subprocess
+import time
+
+while True:
+  subprocess.Popen('sudo airodump-ng wlan0 -w dump', shell=True)
+  time.sleep(900)
+  subprocess.call('sudo pkill airodump-ng', shell=True)
